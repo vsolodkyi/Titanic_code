8c38c0b77d69dfc067ab0736101c688158147d3c
Volodymyr Solodkyi <Volodymyr Solodkyi>
2017-01-08 10:30:39 +0200

    type: create data and numpy objects
    body
    
    footer

--- code.py
+++ code.py
@@ -4,3 +4,17 @@ import numpy as np
 #import csv file
 
 csv_file_object = csv.reader(open("../Titanic.csv", 'rb'))
+
+#escape first line which is header line
+header = csv_file_object.next()
+header[0] = "Id"
+
+#create variable data
+data = []
+
+#add each row wrom csv to data
+
+for row in csv_file_object:
+	data.append(row)
+#convert a data list to array 
+data = np.array(data)
